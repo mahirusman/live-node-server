ea6145635c07f1b2945ee55c556aca0752617652
mahirusman <usmancuiisb@gmail.com>
2022-05-04 16:05:26 +0500

    user

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use("/", (req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 100;
 
 const server = app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
